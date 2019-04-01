153c4d121e52dc191a7522b833376faabfbfbee1
thedocbwarren <jedi.master@augmentedjs.com>
2019-03-31 21:41:42 -0700

    first start

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Frame from "./frame.js";
+
+module.exports = Frame;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  context: __dirname,
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'presentation-frame.js',
+    publicPath: '/dist/',
+    library: "presentation-frame",
+    globalObject: 'this',
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  externals: {
+    'presentation-decorator': {
+      commonjs: 'presentation-decorator',
+      commonjs2: 'presentation-decorator',
+      amd: 'presentation-decorator',
+      root: 'presentation-decorator'
+    }
+  },
+  stats: "errors-only",
+  devtool: "source-map",
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(require("./package.json").version)
+    })
+  ]
+};
